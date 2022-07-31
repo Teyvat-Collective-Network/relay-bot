3de3c1d041fa30c80693a539ddb46dcdd838d811
AroLeaf <starleafofa@gmail.com>
2022-07-31 14:32:06 +0200

    [feat] improved eval command, added message app command version

--- commands/eval.js
+++ commands/eval.js
@@ -1,18 +1,22 @@
 import * as Discord from '@aroleaf/djs-bot';
-import { Event } from '@aroleaf/djs-bot';
+import { SlashCommand } from '@aroleaf/djs-bot';
+import * as util from '../lib/util.js';
 import { inspect } from 'util';
 
-import * as util from '../lib/util.js';
-
-export default new Event({
-  event: 'messageCreate',
-}, async message => {
-  if (!message.content.startsWith('gb!eval') || message.author.id !== process.env.OWNER) return;
+export default new SlashCommand({
+  type: 3,
+  name: 'eval',
+}, async interaction => {
+  const reply = content => interaction.reply({ content, ephemeral: true });
   
+  if (interaction.user.id !== process.env.OWNER) return reply('You are not allowed to use `eval`');
+  
+  const message = interaction.options.resolved.messages.first();
   const codeblocks = message.content.split(/```(?:js)?(.+?)```/sg).filter((_,i) => i%2);
   if (!codeblocks.length) return reply('No code found');
 
   const env = {
+    interaction,
     message,
     Discord,
     util,
@@ -37,8 +41,9 @@ export default new Event({
     numericSeparator: true,
   });
   
-  return message.channel.send({
+  return interaction.reply({
     content: '**eval results**' + (out.length <= 1970 ? `\`\`\`ansi\n${out}\n\`\`\`` : ''),
     files: out.length > 1984 ? [{ name: 'results.ansi', attachment: Buffer.from(out) }] : [],
+    ephemeral: true,
   });
 });
