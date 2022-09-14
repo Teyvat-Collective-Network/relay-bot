43debb3d222841542265ce46f35a64c786c99ae4
AroLeaf <starleafofa@gmail.com>
2022-09-14 20:39:17 +0200

    [fix] use autocomplete instead of modal for

--- commands/connections/connect.js
+++ commands/connections/connect.js
@@ -1,8 +1,30 @@
-import DJS from '@aroleaf/djs-bot';
+import fzy from 'fzy.js';
+import { ApplicationCommandOptionType, SlashCommand } from '@aroleaf/djs-bot';
+import GlobalManager from '../../lib/globalManager.js';
+import * as util from '../../lib/util.js';
 
-export default new DJS.SlashCommand({
+function filter(list, query, map = i => i) {
+  return query ? list.filter(i => fzy.hasMatch(query, map(i))).sort((a, b) => fzy.score(query, map(b)) - fzy.score(query, map(a))) : list;
+}
+
+export default new SlashCommand({
   name: 'connect',
   description: 'connect to a global channel',
+  options: [{
+    type: ApplicationCommandOptionType.String,
+    name: 'channel',
+    description: 'the global channel you want to connect to',
+    required: true,
+    autocomplete: true,
+    onAutocomplete: async interaction => {
+      const globals = await interaction.client.db.Global.find();
+      return interaction.respond(filter(
+        globals,
+        interaction.options.getFocused().toLowerCase(),
+        g => g.name.toLowerCase()
+      ).map(g => ({ name: g.name, value: g.name })).slice(0, 25));
+    },
+  }]
 }, async interaction => {
   const reply = content => interaction.reply({ content, ephemeral: true });
 
@@ -12,16 +34,20 @@ export default new DJS.SlashCommand({
     if (!interaction.memberPermissions.has(1n<<3n)) return reply('Sorry, only TCN execs or admins can manage subscriptions.');
   }
 
-  return interaction.modal({
-    customId: 'modal:connect',
-    title: 'Connect to a global channel',
-    fields: [{
-      customId: 'global',
-      label: 'global channel',
-      placeholder: 'probably `TCN`',
-      value: 'TCN',
-      multiline: false,
-      required: true,
-    }],
-  });
+  const name = interaction.options.getString('channel');
+
+  const global = await interaction.client.db.Global.findOne({ name });
+  if (!global) return reply(`Sorry, ${name} is not a global channel.`);
+
+  const exists = await interaction.client.db.Global.findOne({ subscriptions: interaction.channel.id });
+  if (exists) return reply('Sorry, this channel already has a subscription, please remove it and try again.');
+
+  interaction.channel.operations = new GlobalManager(interaction.channel);
+  await global.updateOne({ $push: { subscriptions: interaction.channel.id } });
+
+  await reply(`Subscription to ${name} added!`);
+
+  util.log(util.fakeMessage(interaction, {
+    content: `${interaction.user} connected ${interaction.channel}`,
+  }), util.tags.connect, global).catch(() => {});
 });
